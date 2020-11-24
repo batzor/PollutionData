d85e6c9bdcffb5ec25cd65a47cf7998ed724a0b2
Azamat Smagulov <s.a.smagulov@gmail.com>
2020-11-24 19:49:42 +0600

    add bootstrap and add home page

--- pollution_service/urls.py
+++ pollution_service/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('recommendation/', views.recommendation, name='recommendation'),
+    path('', views.home, name='home'),
 ]
--- pollution_service/views.py
+++ pollution_service/views.py
@@ -2,11 +2,33 @@ from django.shortcuts import render
 from .api_service import PollutionService
 
 
-def index(request):
-    service = PollutionService()
-    data = service.get_city('seoul')
-    data = data['iaqi']
-    context = {
-        'data': data,
-    }
-    return render(request, 'index.html', context)
+POLLUTANTS = [('pm25', 'PM2.5'), ('pm10', 'PM10'), ('no2', 'NO2'), ('so2', 'SO2'), ('co', 'CO')]
+
+
+def format_pollution_data(data):
+    return [(value, str(data[key]['v']) + ' µg/m³') for key, value in POLLUTANTS]
+
+
+def home(request):
+    context = None
+    error_message = None
+    if request.method == 'POST':
+        try:
+            city = request.POST.get('city_name')
+            service = PollutionService()
+            data = service.get_city(city)
+            data = data['iaqi']
+            context = {
+                'data': format_pollution_data(data),
+                'current_name': city
+            }
+        except:
+            context = {
+                'error_message': 'No city found'
+            }
+    return render(request, 'home.html', context)
+
+
+def recommendation(request):
+    context = None
+    return render(request, 'base.html', context)
